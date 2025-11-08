405e620784eca91c704ec6d6a2038be94786d97b
Jay Ang <zijie0921@gmail.com>
2025-11-07 18:16:39 -0600

    feat: add Swagger API documentation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../config/swagger');
 const v1Routes = require('./v1');
 
 // Health check endpoint
@@ -9,5 +11,8 @@ router.get('/health', (req, res) => {
 
 // API version routes
 router.use('/v1', v1Routes);
+
+// Swagger documentation
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = router;
